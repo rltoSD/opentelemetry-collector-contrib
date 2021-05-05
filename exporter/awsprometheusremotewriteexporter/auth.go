125aea5fa392bf2fb2dbe3b58caf7ece82bb13e3
dhruv-vora <dhruvvora.engg@gmail.com>
2021-05-05 16:14:10 -0700

    precomputing runtime info string and adding to UA Header

--- exporter/awsprometheusremotewriteexporter/auth.go
+++ exporter/awsprometheusremotewriteexporter/auth.go
@@ -36,10 +36,11 @@ const defaultAMPSigV4Service = "aps"
 
 // signingRoundTripper is a Custom RoundTripper that performs AWS Sig V4.
 type signingRoundTripper struct {
-	transport http.RoundTripper
-	signer    *v4.Signer
-	region    string
-	service   string
+	transport    http.RoundTripper
+	signer       *v4.Signer
+	region       string
+	service      string
+	runtime_info string
 }
 
 func (si *signingRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
@@ -57,6 +58,16 @@ func (si *signingRoundTripper) RoundTrip(req *http.Request) (*http.Response, err
 
 	// Clone request to ensure thread safety.
 	req2 := cloneRequest(req)
+
+	// Add the sdk and system information to the User-Agent header of the request
+	// extras := []string{runtime.Version(), runtime.GOOS, runtime.GOARCH}
+	// if v := os.Getenv("AWS_EXECUTION_ENV"); v != "" {
+	// 	extras = append(extras, v)
+	// }
+	// addToUserAgent(req2, aws.SDKName, aws.SDKVersion, extras...)
+	req2.Header.Set("User-Agent", req.Header.Get("User-Agent")+" "+si.runtime_info)
+
+	// Sign the request
 	_, err = si.signer.Sign(req2, body, si.service, si.region, time.Now())
 	if err != nil {
 		return nil, err
@@ -71,7 +82,7 @@ func (si *signingRoundTripper) RoundTrip(req *http.Request) (*http.Response, err
 	return resp, err
 }
 
-func newSigningRoundTripper(cfg *Config, next http.RoundTripper) (http.RoundTripper, error) {
+func newSigningRoundTripper(cfg *Config, next http.RoundTripper, runtime_info string) (http.RoundTripper, error) {
 	auth := cfg.AuthConfig
 	if auth.Region == "" {
 		region, err := parseEndpointRegion(cfg.Config.HTTPClientSettings.Endpoint)
@@ -88,7 +99,7 @@ func newSigningRoundTripper(cfg *Config, next http.RoundTripper) (http.RoundTrip
 	if err != nil {
 		return next, err
 	}
-	return newSigningRoundTripperWithCredentials(auth, creds, next)
+	return newSigningRoundTripperWithCredentials(auth, creds, next, runtime_info)
 }
 
 func getCredsFromConfig(auth AuthConfig) (*credentials.Credentials, error) {
@@ -122,16 +133,17 @@ func parseEndpointRegion(endpoint string) (region string, err error) {
 	return p[1], nil
 }
 
-func newSigningRoundTripperWithCredentials(auth AuthConfig, creds *credentials.Credentials, next http.RoundTripper) (http.RoundTripper, error) {
+func newSigningRoundTripperWithCredentials(auth AuthConfig, creds *credentials.Credentials, next http.RoundTripper, runtime_info string) (http.RoundTripper, error) {
 	if creds == nil {
 		return nil, errors.New("no AWS credentials exist")
 	}
 	signer := v4.NewSigner(creds)
 	rt := signingRoundTripper{
-		transport: next,
-		signer:    signer,
-		region:    auth.Region,
-		service:   auth.Service,
+		transport:    next,
+		signer:       signer,
+		region:       auth.Region,
+		service:      auth.Service,
+		runtime_info: runtime_info,
 	}
 	return &rt, nil
 }
@@ -147,3 +159,20 @@ func cloneRequest(r *http.Request) *http.Request {
 	}
 	return r2
 }
+
+// addToUserAgent will format the provided name and version to the name/version format.
+// If the extra parameters are provided they will be added as metadata to the
+// name/version pair resulting in the following format.
+// "name/version (extra0; extra1; ...)"
+// It then adds the string to the end of the request's current User-Agent.
+// func addToUserAgent(req *http.Request, name, version string, extra ...string) {
+// 	ua := fmt.Sprintf("%s/%s", name, version)
+// 	if len(extra) > 0 {
+// 		ua = fmt.Sprintf("%s (%s)", ua, strings.Join(extra, "; "))
+// 	}
+// 	curUA := req.Header.Get("User-Agent")
+// 	if len(curUA) > 0 {
+// 		ua = curUA + " " + ua
+// 	}
+// 	req.Header.Set("User-Agent", ua)
+// }
